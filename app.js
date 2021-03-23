0fa3afb02134c8daf689d9fd5d9ac8a0882e2703
panggggg <p.picczy@gmail.com>
2021-03-24 00:01:53 +0700

    add edit

---
+++ app.js
@@ -0,0 +1,43 @@
+console.log("Hello Pang")
+console.log("Hello Pang")
+console.log("Hello Pang")
+alert("Hello Pang") //message box
+
+/////////////////////////////////////////////////////
+
+var name = "Pangpicc" //variable
+console.log(name)
+alert(name)
+
+name = "buubie"
+console.log(name)
+alert(name)
+
+/////////////////////////////////////////////////////
+
+var Firstname = "Pawornwan"
+var Lastname = "Sriprathung"
+console.log(Firstname + " " + Lastname)
+alert(Firstname + " " + Lastname)
+
+/////////////////////////////////////////////////////
+
+alert(10) //integer
+var age = 21
+console.log(typeof 21) //typeof เป็นการบอกว่าเป็นตัวแปรชนิดอะไร
+alert(typeof 21)
+
+///////////////////////////////////////////////////// NaN = Not a Number////////
+
+var x = 20
+{
+    var x = 2 // เรียกใช้ที่ไหนก็ได้
+    let y = 10 // let ใช้ได้เฉพาะใน scope เท่านั้น
+    alert(y)
+}
+
+console.log(x)
+
+////////////////////////////////////////////////////////
+
+prompt("Hello World") //prompt เป็นช่องให้userกรอกinputคล้ายๆalert
